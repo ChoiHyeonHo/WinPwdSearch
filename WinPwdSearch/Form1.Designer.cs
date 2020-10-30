a85e735fab511db841e24978352319a6f6bead5f
hyeonho choi <hyunho1232@naver.com>
2020-10-30 15:26:49 +0900

    초기 커밋.

---
+++ WinPwdSearch/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WinPwdSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.txtuid = new System.Windows.Forms.TextBox();
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIDS = new System.Windows.Forms.TextBox();
+            this.txtNameS = new System.Windows.Forms.TextBox();
+            this.txtEmailS = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pnlSearch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "회원ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "비밀번호";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(37, 123);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(97, 36);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "로그인";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnclose
+            // 
+            this.btnclose.Location = new System.Drawing.Point(172, 123);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(97, 36);
+            this.btnclose.TabIndex = 3;
+            this.btnclose.Text = "취소";
+            this.btnclose.UseVisualStyleBackColor = true;
+            // 
+            // txtuid
+            // 
+            this.txtuid.Location = new System.Drawing.Point(100, 34);
+            this.txtuid.Name = "txtuid";
+            this.txtuid.Size = new System.Drawing.Size(169, 25);
+            this.txtuid.TabIndex = 4;
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Location = new System.Drawing.Point(100, 71);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.Size = new System.Drawing.Size(169, 25);
+            this.txtpwd.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.linkLabel1.Location = new System.Drawing.Point(180, 175);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(89, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "비밀번호 찾기";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.linkLabel2.Location = new System.Drawing.Point(34, 175);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(60, 17);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "회원가입";
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.Controls.Add(this.btnCancel);
+            this.pnlSearch.Controls.Add(this.btnSearch);
+            this.pnlSearch.Controls.Add(this.txtEmailS);
+            this.pnlSearch.Controls.Add(this.txtNameS);
+            this.pnlSearch.Controls.Add(this.txtIDS);
+            this.pnlSearch.Controls.Add(this.label5);
+            this.pnlSearch.Controls.Add(this.label4);
+            this.pnlSearch.Controls.Add(this.label3);
+            this.pnlSearch.Location = new System.Drawing.Point(12, 12);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Size = new System.Drawing.Size(261, 208);
+            this.pnlSearch.TabIndex = 8;
+            this.pnlSearch.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Email";
+            // 
+            // txtIDS
+            // 
+            this.txtIDS.Location = new System.Drawing.Point(93, 38);
+            this.txtIDS.Name = "txtIDS";
+            this.txtIDS.Size = new System.Drawing.Size(146, 25);
+            this.txtIDS.TabIndex = 3;
+            // 
+            // txtNameS
+            // 
+            this.txtNameS.Location = new System.Drawing.Point(93, 69);
+            this.txtNameS.Name = "txtNameS";
+            this.txtNameS.Size = new System.Drawing.Size(146, 25);
+            this.txtNameS.TabIndex = 4;
+            // 
+            // txtEmailS
+            // 
+            this.txtEmailS.Location = new System.Drawing.Point(93, 100);
+            this.txtEmailS.Name = "txtEmailS";
+            this.txtEmailS.Size = new System.Drawing.Size(146, 25);
+            this.txtEmailS.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(28, 140);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(112, 29);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "비밀번호 찾기";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(164, 140);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 29);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 232);
+            this.Controls.Add(this.pnlSearch);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtuid);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("나눔바른고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "로그인";
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.TextBox txtuid;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtEmailS;
+        private System.Windows.Forms.TextBox txtNameS;
+        private System.Windows.Forms.TextBox txtIDS;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinPwdSearch/MemberDB.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WinPwdSearch
+{
+    public class Member // 로그인한 멤버의 정보를 담아두고 계속 달고다닌다.
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string IsAdmin { get; set; }
+    }
+
+
+    public class MemberDB : IDisposable
+    {
+        MySqlConnection conn;
+        public MemberDB()//생성자
+        {
+
+            conn = new MySqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["gudi"].ConnectionString;
+            conn.Open();
+        }
+
+
+        public Member Login(string uid, string pwd)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "SELECT ID, Name, Email, Isadmin FROM membertbl where ID = @ID and Pwd = @Pwd;";
+            cmd.Connection = conn;
+
+            cmd.Parameters.Add("@ID", MySqlDbType.VarChar);
+            cmd.Parameters["@ID"].Value = uid;
+
+            cmd.Parameters.Add("@Pwd", MySqlDbType.VarChar);
+            cmd.Parameters["@Pwd"].Value = pwd;
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read()) //로그인 성공시 정보전달
+            {
+                Member member = new Member();
+                member.ID = reader["ID"].ToString();
+                member.Name = reader["Name"].ToString();
+                member.Email = reader["Email"].ToString();
+                member.IsAdmin = reader["Isadmin"].ToString();
+
+                return member;
+            }
+            else //로그인 실패시 null
+            {
+                return null; //리턴 타입이 멤버이기 때문에 없으면 그냥 Null
+            }
+            
+        } //로그인
+
+        public int SearchPwd(string uid, string name, string Email)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = @"select count(*) from membertbl 
+                                where ID = @ID and Name = @Name and Email = @Email;";
+            cmd.Connection = conn;
+
+            cmd.Parameters.Add("@ID", MySqlDbType.VarChar);
+            cmd.Parameters["@ID"].Value = uid;
+
+            cmd.Parameters.Add("@Name", MySqlDbType.VarChar);
+            cmd.Parameters["@Name"].Value = name;
+
+            cmd.Parameters.Add("@Email", MySqlDbType.VarChar);
+            cmd.Parameters["@Email"].Value = Email;
+
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        } //PWD 검증
+
+        public bool UpdatePwd(string newPwd, string uid) // Pwd변경후 메일을 보내야함. 변경성공 여부를 bool타입으로 반환
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = "update membertbl set Pwd = @Pwd where ID = @ID;";
+                cmd.Connection = conn;
+
+                cmd.Parameters.Add("@Pwd", MySqlDbType.VarChar);
+                cmd.Parameters["@Pwd"].Value = newPwd;
+
+                cmd.Parameters.Add("@ID", MySqlDbType.VarChar);
+                cmd.Parameters["@ID"].Value = uid;
+
+                //ExecuteNonQuery는 적용된 Row 수를 반환
+                //정상적인 실행을 했을 때에도 적용된 행의 수에 따라서 다른 결과를 처리할 수 있다.
+                int iRows = cmd.ExecuteNonQuery();
+
+                if (iRows > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception err)
+            {
+                return false;
+            }      
+        }
+
+
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+    }
+}
